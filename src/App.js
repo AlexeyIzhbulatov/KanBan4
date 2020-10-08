67d865edf1565e3dca955774847992540f7a4175
AlexeyIzhbulatov <ouren@mail.ru>
2020-10-08 13:41:23 -0700

    f

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import Board from "./Board";
 
-const initialTask =
+const initialTask = [
+  {id: uuidv4(), name: 'Task1', priority: 0, status: 'Todo'},
+  {id: uuidv4(), name: 'Task2', priority: 1, status: 'Review'},
+  {id: uuidv4(), name: 'Task3', priority: 2, status: 'Progress'},
+  {id: uuidv4(), name: 'Task4', priority: 2, status: 'Done'},
+]
 
 function App() {
+  const [task, setTask] = useState(initialTask)
+
   return (
     <div className="App">
-
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            To do
+            <Board task={task} status='Todo' onChangeStatus={onChangeStatus}/>
+          </div>
+          <div className="col-sm">
+            Review
+            <Board task={task} status='Review' onChangeStatus={onChangeStatus}/>
+          </div>
+          <div className="col-sm">
+            Progress
+            <Board task={task} status='Progress' onChangeStatus={onChangeStatus}/>
+          </div>
+          <div className="col-sm">
+            Done
+            <Board task={task} status='Done' onChangeStatus={onChangeStatus}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
